7740caf6386fd7a09a51f9a5797b6211e0d0cb5d
SiposZoltan63 <siposz@kkszki.hu>
2025-09-10 08:30:55 +0200

    feladat1 is ready

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace OOPAlapok
 {
+    public class Szemely
+    {
+        public string nev = "Zoli";
+        public int kor=18;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine($"A tanuló neve: {tanulo1.nev} és {tanulo1.kor} éves.");
         }
     }
 }
